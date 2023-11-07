2c3c645316d443c4c6d6e4426f9e00c73195af0b
root <root@Kenny.localdomain>
2023-11-07 10:56:17 -0500

    added comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
+//Import all necessary toolkits
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 const uglifycss = require('gulp-uglifycss');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
+//SCSS task - converts CSS into SCSS
 gulp.task('scss', function(){
     return gulp.src('./app/scss/style.scss', {sourcemaps: true})
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./app/css', {sourcemaps: '.'}));
 })
 
+//CSS task - Converts CSS into minified CSS
 gulp.task('css', function () {
     return gulp.src('./app/css/*.css', {sourcemaps: true})
         .pipe(uglifycss({
@@ -18,13 +21,14 @@ gulp.task('css', function () {
         .pipe(gulp.dest('./app/dist/', {sourcemaps: '.'}));
 });
 
-
+//JS task - Converts JS into minified JS file
 gulp.task('js', function () {
     return gulp.src('./app/js/script.js', {sourcemaps: true})
     .pipe(terser())
     .pipe(gulp.dest('./app/dist/', {sourcemaps: '.'}))
 });
 
+//Start Browsersync 
 function browsersyncServe(cb){
     browsersync.init({
         server:{
@@ -34,17 +38,20 @@ function browsersyncServe(cb){
     cb();
 }
 
+//Reload Browsersync 
 function browsersyncReload(cb){
     browsersync.reload();
     cb();
 }
 
 
+//Run all static tasks at the same time
 gulp.task('run', gulp.series('scss', 'css', 'js'));
 
+//Dynamic watch task - watch change in SCSS files
 gulp.task('watch', function () {
     gulp.watch('./app/scss/*.scss', gulp.parallel('scss', browsersyncServe, browsersyncReload));
-    gulp.watch('./app/css/*.css', gulp.parallel('css', browsersyncServe, browsersyncReload));
 });
 
+//Run all tasks
 gulp.task('default', gulp.series('run', 'watch'));
